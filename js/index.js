9f27f2b13a042b06b26fa7e3e5186b53868ad37a
Miro Manestar <miroimanestar@gmail.com>
2021-12-01 19:07:31 -0500

    MORE WORK

--- js/index.js
+++ js/index.js
@@ -4,10 +4,12 @@ var example = (() => {
     let variables = {}
 
     area.onmousedown = e => {
-        const c = getCoords(e)
+        const rect = e.currentTarget.getBoundingClientRect()
+        const x = e.clientX - rect.left
+        let y = e.clientY - rect.top
 
         if (e.button === 0)
-            createBox(c.x, c.y)
+            createBox(x, y)
     }
 
     area.oncontextmenu = e => {
@@ -21,16 +23,12 @@ var example = (() => {
     }
 
     const createBox = (x, y) => {
-        const key = Date.now()
-
         let box = document.createElement('div')
         box.classList.add('box');
-        box.setAttribute('draggable', 'true')
         box.setAttribute('tabindex', '0')
-        box.id = `box-${key}`;
-        box.style.left = x
-        box.style.top = y
         area.appendChild(box)
+        box.style.left = x - box.offsetWidth / 2
+        box.style.top = y - box.offsetHeight / 2
 
         const draggable = new PlainDraggable(box, { onMove: () => {
             for (let li of lines) {
@@ -38,14 +36,10 @@ var example = (() => {
                     li.position()
                 }
             }
-        } })
+        }})
+
+        variables[Date.now()] = con.create()
         
-        variables[key] = con.create()
-
-        box.onmousedown = e => {
-            e.stopPropagation()
-        }
-
         box.onmouseover = e => {
             e.target.focus()
         }
@@ -57,14 +51,51 @@ var example = (() => {
         box.oncontextmenu = e => {
             e.preventDefault()
             e.stopPropagation()
-            console.log('test')
+            
+            let oldVal = box.innerHTML
+            if (box.lastChild && box.lastChild.nodeName === 'INPUT') {
+                if (verifyInput(box.lastChild.value)) {
+                    box.classList.remove('active')
+                    box.innerHTML = box.lastChild.value
+                }
+
+                return
+            }
+
+            let input = document.createElement('input')
+            input.value = box.lastChild ? box.innerHTML : ''
+            box.innerHTML = ''
+            box.classList.add('active')
+            box.appendChild(input)
+            input.focus()
+
+            input.onkeydown = e => {
+                e.stopPropagation()
+                
+                if (e.key === 'Enter') {
+                    if (verifyInput(input.value)) {
+                        box.removeChild(input)
+                        box.classList.remove('active')
+                        box.innerHTML = input.value
+                    }
+                }
+
+                if (e.key === 'Escape') {
+                    box.removeChild(input)
+                    box.classList.remove('active')
+                    box.innerHTML = oldVal
+                }
+            }
+
         }
 
         box.onkeydown = e => {
             e.stopPropagation()
             switch (e.key) {
                 case 'x': deleteBox(e); break;
+                case 'Delete': deleteBox(e); break;
                 case 'f': drawLine(e); break;
+                case 'Backspace': clearSelection(); break;
             }
         }
     }
@@ -102,6 +133,13 @@ var example = (() => {
         e.target.classList.add('selected')
 
         if (line.length === 2) {
+            for (let li of lines) {
+                if ( (line[0] === li.end && line[1] === li.start) || (line[0] === li.start && line[1] === li.end) ) {
+                    clearSelection()
+                    return
+                }
+            }
+
             const temp = new LeaderLine(line[0], line[1], { hide: true })
             temp['show']('draw')
             temp.show()
@@ -121,11 +159,23 @@ var example = (() => {
         line = []
     }
 
-    const getCoords = e => {
-        let rect = e.currentTarget.getBoundingClientRect()
-        let x = e.clientX - rect.left
-        let y = e.clientY - rect.top
+    const verifyInput = input => {
+        const key = Date.now()
 
-        return { x: x, y: y }
+        let val = parseInt(input)
+
+        if (val !== 'NaN') {
+            return true
+        } else {
+            switch (input) {
+                case '+': return true;
+                case '-': return true;
+                case '/': return true;
+                case '*': return true;
+                case '^': return true;
+                case 'sqrt':  console.log('lol'); return true; 
+                default: return false;
+            }
+        }
     }
 })()
